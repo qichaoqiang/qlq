f40956abeae08a2e7862687d25f5605d8f04cc21
qichaoqiang <1543393522@qq.com>
2017-11-01 19:20:26 +0800

    'change'

--- src/pages/home/Header.vue
+++ src/pages/home/Header.vue
@@ -3,7 +3,7 @@
  		<div class="mp-header">
  			<a class="mp-header-left iconfont">&#xe624;</a>
  			<div class="mp-header-title">
- 				<a 	class="mp-linkera">
+ 				<a 	class="mp-linkera">123
  				</a>
  			</div>
  		</div>
