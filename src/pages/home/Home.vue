44651cfdb4ca56cb42b57720bfaccd8bbe5482d6
qichaoqiang <1543393522@qq.com>
2017-11-03 16:56:55 +0800

    'change'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,32 +1,23 @@
 <template>
 	<div>
  		<home-header/>
-<<<<<<< HEAD
  		<weekend-content/>
-=======
  		<swiper-content/>
->>>>>>> dfcd6f5bc30d54e52405aa2661a95aaf0b023469
  	</div>
 </template>
 
 <script>
 
 import HeaderComponent from "./Header";
-<<<<<<< HEAD
-import WeenkendComponent from "./Weekend";
-export default {
-   components: {
-		"home-header": HeaderComponent,
-        "weekend-contend": WeenkendComponent
-=======
 import SwiperComponent from "./Swiper";
+import WeenkendComponent from "./Weekend";
 
 export default {
-	components: {
+   	components: {
 		"home-header": HeaderComponent,
+        "weekend-contend": WeenkendComponent,
 		"swiper-content": SwiperComponent
->>>>>>> dfcd6f5bc30d54e52405aa2661a95aaf0b023469
-	}
+   	}
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
-import Weekend from '@/pages/home/Weekend'
 
 Vue.use(Router)
 
@@ -10,9 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home,
-      name:'Weekend',
-      component:Weekend
+      component: Home
     }
   ]
 })
